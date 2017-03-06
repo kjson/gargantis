b5c4bbc93f1706587d321a876674e92b0fc41126
Kevin Johnson <kevinstuartjohnson@gmail.com>
2017-03-05 20:26:45 -0800

    Added some more system tables

--- example.py
+++ example.py
@@ -8,9 +8,8 @@ import gargantis
 
 def collect_stats(session, interval):
     while True:
-        time.sleep(interval if interval >= 1 else 5)
-        gargantis.pg_stat_activity(session)
-        gargantis.pg_stat_user_tables(session)
+        time.sleep(interval if interval >= 1 else 10)
+        gargantis.insert_all(session)
         session.commit()
 
 
@@ -18,8 +17,12 @@ def main():
     parser = argparse.ArgumentParser(description=__main__.__doc__)
     parser.add_argument('--uri', type=str, help='Database connection string.')
     parser.add_argument('--interval', type=int, help='Data will be logged every @interval secs.')
+    parser.add_argument('--init', action='store_true', help='Init the gargantis tables.')
     parsed = parser.parse_args()
     with postgres.session(parsed.uri) as session:
+        if parsed.init:
+            gargantis.create_all(session)
+            session.commit()
         collect_stats(session, interval if parsed.interval else None)
 
 if __name__ == '__main__':
--- gargantis.py
+++ gargantis.py
@@ -7,41 +7,94 @@ from sqlalchemy import literal
 from postgres import InsertFromSelect, model
 
 
-def create_gargantis_tables(session, contrib=False):
+def create_pg_stat_user_tables(session):
     session.execute('''
-        CREATE SCHEMA gargantis;
-
-        -- pg_stat_activity
-        CREATE TABLE gargantis.pg_stat_activity (
-            like pg_catalog.pg_stat_activity INCLUDING ALL);
-
-        ALTER TABLE gargantis.pg_stat_activity
-            ADD COLUMN date TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc');
-
-        CREATE INDEX ix_pg_stat_activity_date ON gargantis.pg_stat_activity
-            USING btree (date);
+        CREATE SCHEMA IF NOT EXISTS gargantis;
 
         -- pg_stat_user_tables
-        CREATE TABLE gargantis.pg_stat_user_tables (
-            like pg_catalog.pg_stat_user_tables INCLUDING ALL);
+        CREATE TABLE gargantis.pg_stat_user_tables (like pg_catalog.pg_stat_user_tables INCLUDING ALL);
 
         ALTER TABLE gargantis.pg_stat_user_tables
             ADD COLUMN date TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc');
 
-        CREATE INDEX ix_pg_stat_user_tables_date ON gargantis.pg_stat_user_tables
-            USING btree (date);
+        CREATE INDEX ix_pg_stat_user_tables_date ON gargantis.pg_stat_user_tables USING btree (date);
+
         ''')
+
+def create_pg_stat_activity(session):
+    session.execute('''
+        CREATE SCHEMA IF NOT EXISTS gargantis;
+
+        -- pg_stat_activity
+        CREATE TABLE gargantis.pg_stat_activity (like pg_catalog.pg_stat_activity INCLUDING ALL);
+
+        ALTER TABLE gargantis.pg_stat_activity
+            ADD COLUMN date TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc');
+
+        CREATE INDEX ix_pg_stat_activity_date ON gargantis.pg_stat_activity USING btree (date);
+
+        ''')
+
+
+def create_pg_stat_ssl(session):
+    session.execute('''
+        CREATE SCHEMA IF NOT EXISTS gargantis;
+
+        -- pg_stat_ssl
+        CREATE TABLE gargantis.pg_stat_ssl (like pg_catalog.pg_stat_ssl INCLUDING ALL);
+
+        ALTER TABLE gargantis.pg_stat_ssl
+            ADD COLUMN date TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc');
+
+        CREATE INDEX ix_pg_stat_ssl_date ON gargantis.pg_stat_ssl USING btree (date);
+
+        ''')
+
+
+def create_pg_stat_replication(session):
+    session.execute('''
+        CREATE SCHEMA IF NOT EXISTS gargantis;
+
+        -- pg_stat_replication
+        CREATE TABLE gargantis.pg_stat_replication (like pg_catalog.pg_stat_replication INCLUDING ALL);
+
+        ALTER TABLE gargantis.pg_stat_replication
+            ADD COLUMN date TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc');
+
+        CREATE INDEX ix_pg_stat_replication_date ON gargantis.pg_stat_replication USING btree (date);
+
+        ''')
+
+
+def create_pg_buffercache(session):
+    session.execute('''
+        CREATE SCHEMA IF NOT EXISTS gargantis;
+        CREATE EXTENSION IF NOT EXISTS pg_buffercache;
+
+        -- pg_buffercache
+        CREATE TABLE gargantis.pg_buffercache AS SELECT pg_buffercache.* FROM pg_buffercache LIMIT 0;
+
+        ALTER TABLE gargantis.pg_buffercache
+            ADD COLUMN date TIMESTAMP WITHOUT TIME ZONE DEFAULT (NOW() AT TIME ZONE 'utc');
+
+        CREATE INDEX ix_pg_buffercache_date ON gargantis.pg_buffercache USING btree (date);
+
+        ''')
+
+
+def create_all(session):
+    create_pg_stat_user_tables(session)
+    create_pg_stat_activity(session)
+    create_pg_stat_ssl(session)
+    create_pg_stat_replication(session)
+    create_pg_buffercache(session)
 
 
 def generic_catalog(session, pg_catalog_table):
     '''Logs each row of @pg_catalog_table into @gargantis_table at utc time.'''
-    PgCalalog = model(session, 'pg_catalog', pg_catalog_table)
-    Gargantis = model(session, 'gargantis', pg_catalog_table)
-    subquery = (
-        session.
-        query(PgCalalog, literal(datetime.datetime.utcnow()).label('date')).
-        subquery())
-    session.execute(InsertFromSelect(Gargantis.__table__, subquery))
+    pg_catalog = model(session, 'pg_catalog', pg_catalog_table)
+    gargantis = model(session, 'gargantis', pg_catalog_table)
+    session.execute(InsertFromSelect(gargantis.__table__, session.query(pg_catalog).subquery()))
 
 
 def pg_stat_activity(session):
@@ -53,6 +106,27 @@ def pg_stat_user_tables(session):
     generic_catalog(session, 'pg_stat_user_tables')
 
 
+def pg_stat_ssl(session):
+    generic_catalog(session, 'pg_stat_ssl')
+
+
+def pg_stat_replication(session):
+    generic_catalog(session, 'pg_stat_replication')
+
+
+def pg_buffercache(session):
+    session.execute('''
+        INSERT INTO gargantis.pg_buffercache
+        SELECT pg_buffercache.*, NOW() AT TIME ZONE 'utc'
+        FROM pg_buffercache; ''')
+
+
+def insert_all(session):
+    pg_stat_activity(session)
+    pg_stat_user_tables(session)
+    pg_stat_ssl(session)
+    pg_stat_replication(session)
+    pg_buffercache(session)
 
 
 
